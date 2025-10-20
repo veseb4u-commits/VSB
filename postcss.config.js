bbe885f3c74c2a7eeaeb755c06a7d39b5a4d938a
veseb4u-commits <veseb4u@gmail.com>
2025-10-20 16:20:20 -0400

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+3. Commit
+
+---
+
+### **File 5: .gitignore**
+
+1. Add file → `.gitignore`
+2. Paste:
+```
+node_modules
+dist
+dist-ssr
+*.local
+.env
+.env.local
