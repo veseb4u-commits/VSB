d8984a0265e5c5d66cdd8f5e230a36c9ac368342
veseb4u-commits <veseb4u@gmail.com>
2025-10-21 10:27:15 -0400

    Add .gitignore to exclude build and env files
    Added .gitignore file to exclude node_modules and environment files.

--- postcss.config.js
+++ postcss.config.js
@@ -4,20 +4,3 @@ export default {
     autoprefixer: {},
   },
 }
-```
-
-3. Commit
-
----
-
-### **File 5: .gitignore**
-
-1. Add file → `.gitignore`
-2. Paste:
-```
-node_modules
-dist
-dist-ssr
-*.local
-.env
-.env.local
